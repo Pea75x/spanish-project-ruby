fcc9a9eee80e7ac0635577267ceb4330556f52de
Priya <priya_93_x@hotmail.co.uk>
2024-09-29 15:46:11 +0100

    added tests

--- config/initializers/rack-attack.rb
+++ config/initializers/rack-attack.rb
@@ -1,22 +1,37 @@
 class Rack::Attack
-  Rack::Attack.enabled = ENV['ENABLE_RACK_ATTACK'] || Rails.env.production?
-  
-  ### Throttle Spammy Clients ###
-  throttle('req/ip', limit: 300, period: 5.minutes) do |req|
-    req.ip 
-  end
+  if ENV["ENABLE_RACK_ATTACK"] == 'true'
 
-  ### Prevent Brute-Force Login Attacks ###
-  throttle('logins/email', limit: 3, period: 20.seconds) do |req|
-    if req.path == '/auth/login' && req.post?
-      body = JSON.parse(req.body.string) rescue {}
-      body.dig("auth", "email").downcase
+    Rack::Attack.blocklist("blocks unknown routes") do |req|
+      Rack::Attack::Fail2Ban.filter("banlist:#{req.ip}", maxretry: 3, findtime: 10.minutes, bantime: 2.minutes) do
+        if !(Rails.application.routes.recognize_path(req.path, method: req.request_method) rescue false)
+          Rails.logger.info "[Rack::Attack] Blocking request from: #{req.ip} for unknown route: #{req.path}"
+          true
+        end
+      end
     end
-  end
 
-  Rack::Attack.blocklist("blocks unknown routes") do |req|
-    Rack::Attack::Fail2Ban.filter("banlist:#{req.ip}", maxretry: 3, findtime: 10.minutes, bantime: 2.hours) do
-      !Rails.application.routes.recognize_path(req.path, method: req.request_method) rescue false
+    ### Throttle Spammy Clients ###
+    throttle('req/ip', limit: 5, period: 5.minutes) do |req|
+      # Only log when the request is actually throttled
+      Rails.logger.info "[Rack::Attack] Request from: #{req.ip}, route: #{req.path}"
+      req.ip 
+    end
+
+    ### Prevent Brute-Force Login Attacks ###
+    throttle('logins/email', limit: 3, period: 2.minutes) do |req|
+      if req.path == '/auth/login' && req.post?
+        body = JSON.parse(req.body.string) rescue {}
+        body.dig("auth", "username").downcase rescue {}
+      end
+    end
+
+    Rack::Attack.throttled_responder = lambda do |req|
+      now = req.env['rack.attack.match_data'][:period]
+      [429, { 'Content-Type' => 'application/json' }, [{ error: "Throttle limit reached. Try again in #{now} seconds." }.to_json]]
+    end
+
+    Rack::Attack.blocklisted_responder = lambda do |request|
+      [ 503, {}, ['Blocked']]
     end
   end
 end
---
+++ spec/config/rack_attack_spec.rb
@@ -0,0 +1,75 @@
+require "rails_helper"
+require 'support/auth_helper'
+
+describe Rack::Attack, type: :request do
+  include ActiveSupport::Testing::TimeHelpers
+  let!(:user) { create(:user) }
+
+  before do
+    Rack::Attack.enabled = true
+    Rack::Attack.cache.store = ActiveSupport::Cache::MemoryStore.new
+  end
+
+  after do
+    Rack::Attack.enabled = false
+    Rack::Attack.reset!
+  end
+
+  context "throttle IP" do
+    let(:request) { -> { get "/words", headers: { "Accept" => "application/json" }.merge(authenticated_header(user)) } }
+
+    it "blocks the requests after 5 times and returns successful after the trottle period" do
+      5.times do
+        request.call
+
+        expect(response).to have_http_status(:ok)
+      end
+
+      request.call
+
+      expect(response.body).to include("Throttle limit reached. Try again in 300 seconds")
+      expect(response).to have_http_status(:too_many_requests)
+
+      travel_to(5.minutes.from_now) do
+        request.call
+
+        expect(response).to have_http_status(:ok)
+      end
+    end
+  end
+
+  context "throttle login email" do
+    let(:request) { -> {  post "/auth/login", params: {auth: { username: "testuser123", password: "testpassword" }},
+                                              headers: { "Accept" => "application/json" } } }
+
+    it "blocks the requests after 3 times and returns successful after the trottle period" do
+      3.times do
+        request.call
+
+        expect(response).to have_http_status(:ok)
+      end
+
+      request.call
+
+      expect(response.body).to include("Throttle limit reached. Try again in 120 seconds")
+      expect(response).to have_http_status(:too_many_requests)
+
+      travel_to(5.minutes.from_now) do
+        request.call
+
+        expect(response).to have_http_status(:ok)
+      end
+    end
+  end
+
+  context "unknown routes" do
+    context "when the request does not match any route" do
+      it "blocks the request" do
+        get "/word"
+
+        expect(response.status).to eq(503)
+        expect(response.body).to eq("Blocked")
+      end
+    end
+  end
+end
